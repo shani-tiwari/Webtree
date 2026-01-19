16a57fa424e4fff1d68d049c2225a2f3588a38a2
shani-tiwari <shanitiwari2021@gmail.com>
2026-01-19 20:13:24 +0530

    performance increase

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.jac6bu6uhm8"
+    "revision": "0.aqn0oi4bm1"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import Home from "./pages/Home";
-import bgImage from "./assets/webtree-bg.jpg";
 import React from "react";
-
 const Collection = React.lazy(() => import("./pages/Collection"));
 import { Routes, Route } from "react-router";
+import { cn } from "./lib/utils";
 
 function App() {
   return (
@@ -11,15 +10,18 @@ function App() {
       <a href="#main-content" className="skip-link">
         Skip to main content
       </a>
+       <div className="relative flex h-full w-full items-center justify-center bg-white dark:bg-black">
       <div
-        className="relative min-h-screen w-full bg-[--color-bg-main] mx-auto flex justify-center bg-fixed bg-cover bg-center"
-        style={{ backgroundImage: `url(${bgImage})` }}
-      >
-        {/* Subtle dark overlay to ensure readability of text and UI */}
-        <div
-          className="absolute inset-0 bg-neutral-950/40 pointer-events-none"
-          aria-hidden="true"
-        />
+        className={cn(
+          "absolute inset-0",
+          "bg-size-[40px_40px]",
+          "bg-[linear-gradient(to_right,#e4e4e7_1px,transparent_1px),linear-gradient(to_bottom,#e4e4e7_1px,transparent_1px)]",
+          "dark:bg-[linear-gradient(to_right,#262626_1px,transparent_1px),linear-gradient(to_bottom,#262626_1px,transparent_1px)]",
+        )}
+      />
+      {/* Radial gradient for the container to give a faded look */}
+      <div className="pointer-events-none abso lute inset-0 flex items-center justify-center bg-white mask-[radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black"></div>
+
 
         <div className="relative z-10 w-full flex justify-center">
           <React.Suspense
