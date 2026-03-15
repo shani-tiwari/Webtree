c64294e16f0f0b2e87afd7c3685a8dbea409e801
shani-tiwari <shanitiwari2021@gmail.com>
2026-03-16 00:02:50 +0530

    new icons - pwa

--- vite.config.js
+++ vite.config.js
@@ -18,58 +18,56 @@ export default defineConfig({
       registerType: "autoUpdate",
       devOptions: { enabled: true },
       manifest: {
-        name: "WebTree",
-        short_name: "WebTree",
-        description: "WebTree",
-        theme_color: "#000000",
+        name: "webtree",
+        short_name: "webtree",
         icons: [
           {
-            src: "icons/an round tree with open to up and close in bottom-48x48.png",
+            src: "icons/icon-48x48.png",
             sizes: "48x48",
             type: "image/png",
           },
           {
-            src: "icons/an round tree with open to up and close in bottom-72x72.png",
+            src: "icons/icon-72x72.png",
             sizes: "72x72",
             type: "image/png",
           },
           {
-            src: "icons/an round tree with open to up and close in bottom-96x96.png",
+            src: "icons/icon-96x96.png",
             sizes: "96x96",
             type: "image/png",
           },
           {
-            src: "icons/an round tree with open to up and close in bottom-128x128.png",
+            src: "icons/icon-128x128.png",
             sizes: "128x128",
             type: "image/png",
           },
           {
-            src: "icons/an round tree with open to up and close in bottom-144x144.png",
+            src: "icons/icon-144x144.png",
             sizes: "144x144",
             type: "image/png",
           },
           {
-            src: "icons/an round tree with open to up and close in bottom-152x152.png",
+            src: "icons/icon-152x152.png",
             sizes: "152x152",
             type: "image/png",
           },
           {
-            src: "icons/an round tree with open to up and close in bottom-192x192.png",
+            src: "icons/icon-192x192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "icons/an round tree with open to up and close in bottom-256x256.png",
+            src: "icons/icon-256x256.png",
             sizes: "256x256",
             type: "image/png",
           },
           {
-            src: "icons/an round tree with open to up and close in bottom-384x384.png",
+            src: "icons/icon-384x384.png",
             sizes: "384x384",
             type: "image/png",
           },
           {
-            src: "icons/an round tree with open to up and close in bottom-512x512.png",
+            src: "icons/icon-512x512.png",
             sizes: "512x512",
             type: "image/png",
           },
@@ -77,9 +75,14 @@ export default defineConfig({
         start_url: "/",
         display: "standalone",
         background_color: "#ffffff",
+        theme_color: "#000000",
       },
       workbox: {
-        navigateFallbackDenylist: [/^\/sitemap\.xml$/, /^\/robots\.txt$/, /^\/1ead026798054782827bba53da149021\.txt$/],
+        navigateFallbackDenylist: [
+          /^\/sitemap\.xml$/,
+          /^\/robots\.txt$/,
+          /^\/1ead026798054782827bba53da149021\.txt$/,
+        ],
         // logo caching
         runtimeCaching: [
           {
