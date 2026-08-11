28b42e94c9a8fd1c6e43a378771f951f483d7dcc
shani-tiwari <shanitiwari2021@gmail.com>
2026-08-11 17:44:58 +0530

    new website - web design category

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-ca84f546'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.htl56o8kkro"
+    "revision": "0.ckb9bro5iqg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
