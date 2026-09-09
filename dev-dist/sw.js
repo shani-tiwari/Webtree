57aeae29a93d8785b5fb607f3336b4be68d6a0c1
shani-tiwari <shanitiwari2021@gmail.com>
2026-09-09 11:25:49 +0530

    add: audio website

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-ca84f546'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.i0su2sq4ev"
+    "revision": "0.ehi36863qeo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
