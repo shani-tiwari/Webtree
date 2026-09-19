e9b5defa6963cac712bc53f9645492446780284d
shani-tiwari <shanitiwari2021@gmail.com>
2026-09-19 18:28:09 +0530

    new: feral UI(bg)

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-ca84f546'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.ierug57sde4"
+    "revision": "0.69ii5t30hs8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
